cfecf72ec98075bb29d526f14dc5f358a472819d
arampacha <69807323+arampacha@users.noreply.github.com>
2021-01-17 02:01:01 +0200

    distrib +argparse (#82)

--- distrib.py
+++ distrib.py
@@ -1,18 +1,21 @@
 from fastai.basics import *
-from .all import *
-from .expscript import *
+from reformer_fastai.all import *
+from reformer_fastai.expscript import *
 from fastai.distributed import *
 import time
 
-path = rank0_first(download_enwik8_data())
-dls = get_enwik8_dataloader(data_path='./data', bs=8, sl=2048, n_workers=None)
+print('Loading data...')
+path = rank0_first(download_enwik8_data)
+print('Preparing dataloaders...')
+dls = rank0_first(get_enwik8_dataloader, data_path='./data', bs=4, sl=512, n_workers=4)
 config = TransformerLMConfigEnwik8(warn=False, verbose=False)
 model = TransformerLM.from_config(config)
-learn = get_lm_learner(dls, model)
-
+learn = rank0_first(get_lm_learner, dls, model)
+cbs = []
 # wandb_run, cbs = init_wandb(cbs, wandb_name=run_name, wandb_group=wandb_group, 
 #                             wandb_notes=wandb_notes, wandb_tags=wandb_tags)
-with learn.distrib_ctx(): learn.fit(1)
+print('Training...')
+with learn.distrib_ctx(): learn.fit(1, cbs=cbs)
 
 save_model = False
 if save_model:
