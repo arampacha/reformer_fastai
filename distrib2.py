50aa5ac4b8e93195ac34242767d6ddaa2dd28fa0
Arto <aruthart@gmail.com>
2021-01-18 14:36:32 +0200

    fix distrib2

--- distrib2.py
+++ distrib2.py
@@ -12,9 +12,9 @@ def run_exp(
     # lr:Param(help="Learning rate", type=float, default=1e-3),
     bs:         Param(help="Batch size", type=int, default=4),
     sl:         Param(help="Sequence length", type=int, default=512),
-    max_seq_len:Param(help="Max sequence length for model embedding and dataloader", type=int, default=512),
-    axial_shape:Param(help="Axial Positional Encoding shape, passed as a string, e.g. '64,32''", type=str, default='32,16'),
-    wandb_log:  Param(help="Use weights and biases logging", type=bool_arg, default=False),
+    max_seq_len:Param(help="Max sequence length for model embedding and dataloader", type=int, default=8192),
+    axial_shape:Param(help="Axial Positional Encoding shape, passed as a string, e.g. '64,32''", type=str, default='128,64'),
+    do_wandb_logging:  Param(help="Use weights and biases logging", type=bool_arg, default=False),
     run_name:   Param(help="Run name for wandb tracking and model filename", type=str, default=''),
     wandb_group:Param(help="wandb group", type=str, default='TEST'),
     wandb_notes:Param(help="wandb notes", type=str, default='My experiment notes'),
@@ -26,10 +26,10 @@ def run_exp(
     path = rank0_first(download_enwik8_data, dest=data_path)
     print('Preparing dataloaders...')
     dls = rank0_first(get_enwik8_dataloader, data_path=data_path, bs=bs, sl=sl, n_workers=None)
-    config = TransformerLMConfigEnwik8(warn=False, verbose=False, max_seq_len=max_seq_len, axial_shape=axial_shape,
-                                    )
+    axial_shape = tuple(map(int, axial_shape.split(',')))
+    config = TransformerLMConfigEnwik8(warn=False, verbose=True, max_seq_len=max_seq_len, axial_shape=axial_shape)
     model = TransformerLM.from_config(config)
-    learn = rank0_first(get_lm_learner, dls, model)
+    learn = get_lm_learner(dls, model)
     cbs = []
     if do_wandb_logging:
         wandb_run, cbs = init_wandb(cbs, wandb_name=run_name, wandb_group=wandb_group, 
